3d44d9eb2ad47eb6f41a84a092eb4b1066391b44
Tobias Ulrich <tobiasbulrich@gmail.com>
2024-09-30 00:48:11 -0300

    Add css plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,18 @@
+import plugins from './src/config/plugins.js';
+
+const INPUT_DIR = "src";
+
 /**
  * @param {import("@11ty/eleventy/UserConfig")} eleventyConfig -
  * @returns {object}
  */
 export default async function(eleventyConfig) {
+  // watch folders
+  eleventyConfig.addWatchTarget(`${INPUT_DIR}/styles/**/*.{css}`); 
+
+  // add plugins
+  eleventyConfig.addPlugin(plugins.cssConfig);
+
   return {
     dir: {
       input: "src",
