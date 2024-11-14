fc85638f7e9604c037b40c034cc7164b8bf0f75a
Tobias Ulrich <tobiasbulrich@gmail.com>
2024-11-14 01:56:40 -0300

    Add split and contains filters

---
+++ src/config/filters/contains.js
@@ -0,0 +1,25 @@
+
+/**
+ * @param {string | number} value 
+ * @param {string | Array<string>} needle 
+ * @returns {boolean}
+ */
+export default function contains(value, needle = "") {
+  if (Array.isArray(needle)) {
+    for (let i = 0; i < needle.length; i++) {
+      const element = needle[i];
+      if (value.includes(element)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  if (typeof value === "number") {
+    value = `${value}`;
+  }
+
+
+  return value.includes(needle);
+}
---
+++ src/config/filters/split.js
@@ -0,0 +1,10 @@
+
+/**
+ * Split string.
+ * @param {string} value 
+ * @param {string | number} separator 
+ */
+export default function split(value, separator) {
+  const r = value.split(separator);
+  return r;
+}
