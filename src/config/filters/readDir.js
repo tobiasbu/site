4e873a0cb3de464c084f466940796d401c873cda
Tobias Ulrich <tobiasbulrich@gmail.com>
2024-11-24 19:58:25 -0300

    Add filter for readDir filter

--- src/config/filters/readDir.js
+++ src/config/filters/readDir.js
@@ -2,12 +2,23 @@ import { readdirSync } from 'node:fs';
 
 /**
  * Split string.
- * @param {string} filePath 
+ * @param {string} filePath
+ * @param {string} [filter=""]
  */
-export default function readDir(filePath) {
+export default function readDir(filePath, filter = "") {
   try {
-    const files = readdirSync(filePath);
-    const outputPath = filePath.replace("src", "")
+    let files = readdirSync(filePath);
+
+    if (filter && filter.length > 0) {
+      files = files.reduce((result, current) => {
+        if (current.includes(filter)) {
+          result.push(current);
+        }
+        return result;
+      }, []);
+    }
+
+    const outputPath = filePath.replace("src", "");
     return files.map((f) => `${outputPath}/${f}`);
   } catch (err) {
     return [err];
