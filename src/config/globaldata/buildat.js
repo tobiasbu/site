b9a8bc7ac85487f3c732b131ef2217011c041347
Tobias Ulrich <tobiasbulrich@gmail.com>
2025-01-03 00:50:16 -0300

    chore: update copyright year

--- src/config/globaldata/buildat.js
+++ src/config/globaldata/buildat.js
@@ -1,8 +1,7 @@
 
 
-export default function buildAt(eleventyConfig) {
-  eleventyConfig.addGlobalData('tob.builtAt', () => {
-    const timeZone =  "America/Sao_Paulo";
+function getDateNow() {
+  const timeZone =  "America/Sao_Paulo";
 
     const now = new Date();
     const time = new Intl.DateTimeFormat(
@@ -10,7 +9,10 @@ export default function buildAt(eleventyConfig) {
       { timeZone, dateStyle: 'full', timeStyle: 'long',  }
     );
     
-    return { timeZone, isoString: now.toLocaleString('sv'), long: time.format(now), }
-  });
+    return { timeZone, isoString: now.toLocaleString('sv'), long: time.format(now), year: now.getFullYear() }
+}
+
+export default function buildAt(eleventyConfig) {
+  eleventyConfig.addGlobalData('tob.builtAt', getDateNow);
 }
 
